73110fdb9e1c6d46b81496690d69cc10a8dc88c1
Wanja Runkel <wanja.runkel@gmail.com>
2018-08-05 13:34:00 +0200

    add button that creates a list of Coordinates and logs it to the screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,9 @@ import { Geolocation } from '@ionic-native/geolocation';
 export class HomePage {
   lat: any;
   lng: any;
+  listOfCoordinates: [] = [];
+
+  // fahrten: Fahrt[];
 
   constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
@@ -18,6 +21,15 @@ export class HomePage {
     this.geolocation.getCurrentPosition().then( pos => {
       this.lat = pos.coords.latitude;
       this.lng = pos.coords.longitude;
+    }).catch( err => console.log(err));
+  }
+
+  getGeo() {
+    this.geolocation.getCurrentPosition().then( pos => {
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+      this.listOfCoordinates.push("lat: " + this.lat + ", lng: " + this.lng + ".");
+      console.log("gelocation: " + this.listOfCoordinates);
     }).catch( err => console.log(err));
   }
 
